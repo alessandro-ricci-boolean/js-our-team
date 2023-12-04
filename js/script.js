989f85bec6351c79ce42ce323bea53f65551d069
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-12-04 18:16:53 +0100

    added basic structure

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let team = [
+    {
+        "nome" : "Wayne",
+        "cognome" : "Barnett",
+        "ruolo" : "Founder & CEO",
+        "foto" : "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        "nome" : "Angela",
+        "cognome" : "Caroll",
+        "ruolo" : "Chief Editor",
+        "foto" : "angela-caroll-chief-editor.jpg",
+    },
+    {
+        "nome" : "Walter",
+        "cognome" : "Gordon",
+        "ruolo" : "Office Manager",
+        "foto" : "walter-gordon-office-manager.jpg",
+    },
+    {
+        "nome" : "Angela",
+        "cognome" : "Lopez",
+        "ruolo" : "Social Media Manager",
+        "foto" : "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        "nome" : "Scott",
+        "cognome" : "Estrada",
+        "ruolo" : "Developer",
+        "foto" : "scott-estrada-developer.jpg",
+    },
+    {
+        "nome" : "Barbara",
+        "cognome" : "Ramos",
+        "ruolo" : "Graphic Designer",
+        "foto" : "barbara-ramos-graphic-designer.jpg",
+    }
+]
+console.log(team)
